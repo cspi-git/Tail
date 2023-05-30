e011a01e72a940d2f9dcd3109dca55b6cbd39077
iUseYahoo <logicatedlzyt@gmail.com>
2023-05-31 02:55:41 +1000

    Tail - Disable fullscreen and resizing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.net.URL;
 import java.util.Set;
 import javax.swing.*;
@@ -19,6 +21,7 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         stage.setTitle("Hanaui - Tail");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
